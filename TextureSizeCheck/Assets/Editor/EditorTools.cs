a6bd80149da887c07ba4702f8b9ff18f5d4dbace
moon <109827298@qq.com>
2019-08-13 17:52:03 +0800

    add files

---
+++ TextureSizeCheck/Assets/Editor/EditorTools.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EditorTools{
+
+    private static string defaultWhiteTexPath_relative = "Assets/Default_Alpha.png";
+
+    private static Texture2D defaultWhiteTex = null;
+
+
+    [MenuItem("EffortForETC1/Depart RGB and Alpha Channel")]
+    public static void SeperateAllTexturesRGBandAlphaChannel()
+    {
+        Debug.Log("HERE");
+
+
+        Debug.Log("Start Departing.");
+        if (!GetDefaultWhiteTexture())
+        {
+            return;
+        }
+
+        
+    }
+
+    public static bool GetDefaultWhiteTexture()
+    {
+        defaultWhiteTex = AssetDatabase.LoadAssetAtPath(defaultWhiteTexPath_relative, typeof(Texture2D)) as Texture2D;  //not just the textures under Resources file  
+        if (!defaultWhiteTex)
+        {
+            Debug.LogError("Load Texture Failed : " + defaultWhiteTexPath_relative);
+            return false;
+        }
+        return true;
+    }
+}
